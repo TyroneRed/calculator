a8dbb2ff857347cce581ff620fdebae91ae35e47
Maureen Hyde <maureenhyde@gmail.com>
2024-04-15 11:27:59 -0400

    Refactor code

--- javascript.js
+++ javascript.js
@@ -3,124 +3,85 @@ let str1 = '';
 let operator = '';
 let otherButtonClicked = 'false';
 
-const numberButtons = document.querySelectorAll('.num-btn');
-numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener("click",function getInput(){
-        console.log(otherButtonClicked);
-        if (str2 === '0'){
-            str2 = ''
-        }    
-        additionalCharacter = numberButton.textContent    
-        if (additionalCharacter === '.'){
-            if (str2.includes('.')){
-                additionalCharacter = '';
-            }
-            if (str2 == ''){
-                additionalCharacter = '0.'
-            }
-        }    
-        if ((additionalCharacter === '0' && str2 == '') || (otherButtonClicked === 'true')){
-            additionalCharacter = ''; 
-        }
+//Listen for input at each of the buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener("click", getInput);
+})
+ 
+//Handle button input
+function getInput(event) {
+    button = event.target;
+
+    // Create and display output when number button pressed
+    if (button.className === 'num-btn'){
+        str2 = createOutput(button.textContent);
+        displayOutput(str2);
+
+    // Handle calculations and output when + - / * or = pressed        
+    }else if (button.className === 'operator'){
     
-        if (str2.length < 8){
-        str2 += additionalCharacter;
-        displayOutput(str2);  
-        }
-    } );
-});
-
-
-
-const operatorButtons = document.querySelectorAll('.operator');
-operatorButtons.forEach((operatorButton) => {
-    operatorButton.addEventListener("click", function getOperator(){
         otherButtonClicked = 'false';
-        operate();
-        operator = operatorButton.id; 
+        result = operate(operator);
+        if (result !== ''){
+        displayOutput(String(result));
+        } 
+        operator = button.id;
         if (str2 !== ''){
         str1 = str2;
         str2 = '';
-        }    
-    });
-});
+        }      
+    // Handle calculations and output when ! % or +/- pressed   
+    }else if (button.className === 'other'){
+        result = handleOtherButton(button.id);
+        displayOutput(String(result));
+    }
 
-// Display output to 8 characters, round decimals, show errors when output too long
-function displayOutput(str){
-    let output = document.querySelector('#output');
-    if (str.length > 8 && str.includes('.') && !str.includes('e')){
-        let stringArr = str.split('.');
-        let length = stringArr[0].length;
-        let power = 7 - length; 
-        let rounded = Math.round(Number(str)*(10**power))/(10**power);
-        output.textContent = String(rounded);    
-    }else if (str.length > 8 || str == 'Infinity' || str == 'NaN'){
-  
-        output.textContent = 'ERROR';
-
-    }else{ 
-        output.textContent = str;
-    };
+    // Clear screen when CE button pressed
+    else if (button.className === 'clear'){
+        clearDisplay();
+    }
 };
 
-//Clear the display, reset variables 
-function clearDisplay(){
+// Add characters to string when numbers or . pressed
+
+function createOutput(character){
+    //Don't add extra zeros at the front
+    if (str2 === '0'){
+        str2 = ''
+    }   
     
-    str2 = '0';
-    str1 = '';
-    operator = '';
-    displayOutput(str2);
-    otherButtonClicked = 'false';
+    // Don't allow extra decimals
+    if (character === '.'){
+        if (str2.includes('.')){
+            character = '';
+        }
+        // Add a zero before a starting decimal
+        if (str2 == ''){
+            character = '0.';
+        }
+    }    
+    // Don't add extra zeros, don't allow the addition of characters after !, % or +/- pressed
+    if ((character === '0' && str2 == '') || (otherButtonClicked === 'true')){
+        character = ''; 
+    }
+
+    // Limit string length for display purposes
+    if (str2.length < 8){
+    str2 += character;
+    }
+    return str2;
 };
-
-let clearButton = document.querySelector('#CE')
-clearButton.addEventListener("click", clearDisplay);
-
-
-let equalsButton = document.querySelector("#equals");
-equalsButton.addEventListener("click",  operate)
-
-
-function operate(){
-    num1 = Number(str1);
-    num2 = Number(str2);
-    otherButtonClicked = 'false';
-    if (str2 !== '' && str1 !== '' && operator !== ''){
-    let result;
-    switch(operator){
-        case 'plus':
-            result = num1 + num2;
-            break;
-        case 'minus':
-            result = num1 - num2;
-            break;
-        case 'times':
-            result = num1 * num2;
-            break;
-        case 'divide':
-            result = num1 / num2;
-            break;
-    };
-
-    displayOutput(String(result));
-    str1 = String(result);
-    str2 = '';//prevents operating again 
-    operator = '';
-}
     
-};
 
-let otherButtons = document.querySelectorAll(".other");
-otherButtons.forEach((otherButton) => {
-    otherButton.addEventListener("click", function handleOtherButtons() {
-        
-        
-        let result = 1;
+// Calculate and output immediately when !, % or +/- pressed 
+function handleOtherButton(otherButton){
+    let result = 1;
         if (str2 === ''){
             str2 = str1;
         }
         if (str2){
-        switch (otherButton.id){
+        switch (otherButton){
             
             case 'factorial': 
                 num2 = Number(str2);
@@ -146,11 +107,76 @@ otherButtons.forEach((otherButton) => {
             }  
             
       
-        displayOutput(String(result)); 
+         
         str2 = String(result);  
-        otherButtonClicked = 'true';      
+        otherButtonClicked = 'true'; 
+        return result;     
     };
-    });   
+}
+
+// display output
+function displayOutput(str){
+    let output = document.querySelector('#output');
     
-});        
+    // Round decimals to 8 total characters
+    if (str.length > 8 && str.includes('.') && !str.includes('e')){
+        let stringArr = str.split('.');
+        let length = stringArr[0].length;
+        let power = 7 - length; 
+        let rounded = Math.round(Number(str)*(10**power))/(10**power);
+        output.textContent = String(rounded);
+
+    // Display ERROR message for calculations resulting in greater than 8 digits and division by 0    
+    }else if (str.length > 8 || str == 'Infinity' || str == 'NaN'){
+  
+        output.textContent = 'ERROR';
+
+    }else{ 
+        output.textContent = str;
+    };
+};
+
+//Clear the display, reset variables 
+function clearDisplay(){
+    
+    str2 = '0';
+    str1 = '';
+    operator = '';
+    displayOutput(str2);
+    otherButtonClicked = 'false';
+};
+
+
+
+// Calculate and Display results
+function operate(){
+    num1 = Number(str1);
+    num2 = Number(str2);
+    otherButtonClicked = 'false';
+    let result = '';
+    if (str2 !== '' && str1 !== '' && operator !== ''){
         
+        switch(operator){
+            case 'plus':
+                result = num1 + num2;
+                break;
+            case 'minus':
+                result = num1 - num2;
+                break;
+            case 'times':
+                result = num1 * num2;
+                break;
+            case 'divide':
+                result = num1 / num2;
+                break;
+        };
+        str1 = String(result);
+        str2 = '';//prevents operating again 
+        operator = '';
+    }
+
+return result;    
+};
+    
+
+
